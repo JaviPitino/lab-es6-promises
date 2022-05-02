ec40ef351e6ce29540ff4c8f6ba9da6cffc54b80
Sarah <sarahjanecalvoconesa@gmail.com>
2022-05-02 18:54:38 +0200

    bonus1

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@
 
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
-// Out of sync
+//Out of sync
   getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
   }, (error) => console.log(error));
@@ -25,9 +25,11 @@
     document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
   }, (error) => console.log(error));
 
+  document.querySelector("#mashedPotatoesImg").hidden= false
 
 
-// Iteration 1 - using callbacks
+
+// // Iteration 1 - using callbacks
 
 
 // Iteration 2 - using promises
@@ -66,9 +68,47 @@ obtainInstruction('steak', 0)
     // return obtainInstruction(`steak`, 8)
   })
 
+  document.querySelector("#steakImg").hidden= false
 
-// Iteration 3 using async/await
-// ...
 
-// Bonus 2 - Promise all
+//Iteration 3 using async/await
+//...
+
+async function makeBroccoli() {
+  try {
+    let broc1 = await obtainInstruction('broccoli', 0);
+    document.querySelector('#broccoli').innerHTML += `<li>${broc1}</li>`
+
+    let broc2 = await obtainInstruction('broccoli',1);
+    document.querySelector('#broccoli').innerHTML += `<li>${broc2}</li>`
+
+    let broc3 = await obtainInstruction('broccoli',2);
+    document.querySelector('#broccoli').innerHTML += `<li>${broc3}</li>`
+
+    let broc4 = await obtainInstruction('broccoli',3);
+    document.querySelector('#broccoli').innerHTML += `<li>${broc4}</li>`
+
+    let broc5 = await obtainInstruction('broccoli',4);
+    document.querySelector('#broccoli').innerHTML += `<li>${broc5}</li>`
+
+    let broc6 = await obtainInstruction('broccoli',5);
+    document.querySelector('#broccoli').innerHTML += `<li>${broc6}</li>`
+
+    let broc7 = await obtainInstruction('broccoli',6);
+    document.querySelector('#broccoli').innerHTML += `<li>${broc7}</li>`
+    document.querySelector('#broccoli').innerHTML += `<li>Broccoli is Ready!</li>`
+  }
+  catch(error) {
+    console.log(error)
+  }
+}
+makeBroccoli()
+
+document.querySelector("#broccoliImg").hidden= false
+
+// Bonus 1 - 
+
+
+document.querySelector("#brusselsSproutsImg").hidden= false
+
 // ...
